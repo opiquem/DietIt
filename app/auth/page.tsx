8cd8ae15cc0ea3a149d7241174203259e15cf605
opiquem <maxbobryk.dev@gmail.com>
2024-01-05 21:21:26 +0200

    initial commit

---
+++ app/auth/page.tsx
@@ -0,0 +1,17 @@
+'use client'
+import Link from 'next/link';
+import React, { FC, useState } from 'react';
+
+export default function AuthPage() {
+  const [number, setNumber] = useState(0);
+  const increase = () => {
+    setNumber((lastNumber) => lastNumber + 1)
+  }
+  return (
+    <div>
+      <button onClick={increase}>Click</button>
+      <p>{number}</p>
+      <Link href={'/'}>Home</Link>
+    </div>
+  );
+};
